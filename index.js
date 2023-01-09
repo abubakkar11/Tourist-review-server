072da05514633d83836fb9efea7987d5f3fc71b5
abubakkar11 <abubakkar995@gmail.com>
2023-01-09 06:43:49 -0800

    updated

--- index.js
+++ index.js
@@ -33,11 +33,6 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET , {expiresIn:'1h'})
       res.send({token})
     })
-    // app.post('/jwt' , (req , res)=>{
-    //   const user = req.body;
-    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET , {expiresIn:'1h'})
-    //   res.send({token})
-    // })
     app.get('/all-services', async (req, res) => {
       const query = {}
       const cursor = serviceCollection.find(query)
